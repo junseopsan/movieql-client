af7f563c21323bf3bd587e72e2b8f69d0619ad34
JunseopPark <metternichch@gmail.com>
2019-09-18 16:19:25 +0900

    init commit

---
+++ src/apolloClient.js
@@ -0,0 +1,8 @@
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+  //url : "https://movieql.now.sh/"
+});
+
+export default client;
