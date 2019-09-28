408a5b0c000282d393d9311ba13e1b40e0509a3b
JunseopPark <metternichch@gmail.com>
2019-09-28 17:38:37 +0900

    Creating a Query with variables
    아폴로 캐시기능은 어마어마하다, 리덕스를 사용할 필요가 없다

--- src/queries.js
+++ src/queries.js
@@ -13,3 +13,21 @@ export const HOME_PAGE = gql`
     }
   }
 `;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      description_intro
+      id
+      title
+      genres
+      medium_cover_image
+      rating
+    }
+    suggestions(id: $movieId) {
+      title
+      rating
+      medium_cover_image
+    }
+  }
+`;
